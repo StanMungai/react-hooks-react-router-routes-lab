e2d1999150d69ad2243a2b070d1719b2468451a8
stanford kangethe <stanfordkangethe16@gmail.com>
2023-07-11 06:31:15 +0300

    Worked on react routes

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      { movies.map((movie) => {
+        return (
+          <div>
+            {movie.title}
+            {movie.time}
+            { movie.genres.map((genre) => {
+              return (
+                <ul>
+                  <li>{genre}</li>
+                </ul>
+              )
+            }) }
+          </div>
+        )
+      }) }
+    </div>
+  );
 }
 
 export default Movies;
